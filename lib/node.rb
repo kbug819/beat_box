2d32f3dd02dc90718b16b30e8551fbe0a192aecd
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-07-06 18:01:15 -0500

    Add first initialize tests

---
+++ lib/node.rb
@@ -0,0 +1,10 @@
+class Node
+  attr_reader :data, :next_node
+  # attr_accessor: :next_node
+  def initialize (data)
+    @data = data
+    @next_node = next_node
+  end
+
+
+end
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,15 +1,31 @@
 require './lib/node'
+node = Node.new("plop")
 
 
-# describe Student do
-#   describe '#initialize' do
-#     it 'is an instance of student' do
-#     #write the code to initialize a new studnt object
-#     student = Student.new('Penelope')  
+describe Node do
+  describe '#initialize' do
+    it 'is an instance of node' do
 
-#     expect(student).to be_an_instance_of(Student)
-#     end
-#   end
+    # node = Node.new("plop")
+    expect(node).to be_an_instance_of(Node)
+    end
+  end
+  describe "#data" do
+    it "returns the data" do
+
+    expect(node.data).to eq("plop")
+    end
+  end
+   describe "#next_node" do
+    it "returns next_node" do
+
+    expect(node.next_node).to eq(nil)
+    end
+  end
+
+
+
+end
 
 #   describe "name" do
 #     it "returns the name of the student" do
