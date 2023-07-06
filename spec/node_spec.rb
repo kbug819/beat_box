c852cccc139c81258da5b4418638df2a5271d947
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-07-06 17:45:01 -0500

    Complete file setup

---
+++ spec/node_spec.rb
@@ -0,0 +1,45 @@
+require './lib/node'
+
+
+# describe Student do
+#   describe '#initialize' do
+#     it 'is an instance of student' do
+#     #write the code to initialize a new studnt object
+#     student = Student.new('Penelope')  
+
+#     expect(student).to be_an_instance_of(Student)
+#     end
+#   end
+
+#   describe "name" do
+#     it "returns the name of the student" do
+    
+#     student = Student.new('Penelope')  
+    
+#     expect(student.name).to eq("Penelope")
+#     end
+#   end
+
+#   describe "#cookies" do
+#     it "returns an array of cookies" do
+
+#     student = Student.new('Penelope') 
+
+#     expect(student.cookies).to eq([])
+#     end
+#   end
+
+#   describe "#add_cookie" do
+#     it "returns added cookies to cookies array" do
+
+#       student = Student.new('Penelope')
+#       student.add_cookie('Chocolate Chunk')
+#       expect(student.cookies).to eq(['Chocolate Chunk'])
+#       student.add_cookie('Snikerdoodle')
+#       expect(student.cookies).to eq(['Chocolate Chunk', 'Snikerdoodle'])
+
+#       # expect(student.)
+#     end
+#   end
+
+# end
