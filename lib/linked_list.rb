948619d7544580a3a0121a3999e3d683d3128673
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-07-08 19:36:50 -0500

    Add iteration one

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,32 +2,98 @@ class LinkedList
   attr_reader :head
   def initialize
     @head = nil
-  end
+ end
 
 
   def append(data)
     new_node = Node.new(data)
+    current = @head
 
     if @head == nil
       @head = new_node
     else
-      if next_node == nil
-        next_node = new_node
+      current = @head 
+      if current.next_node == nil
+        current.next_node = new_node 
+      end
     # if @head = nil?
     #   @head = node_1
     # end
     end
+    # @count_node += 1
   end 
 
-  def count #This method does not seem to be counting the length
-    if @head.data == nil
-      0
-    else
-      1
+  def count 
+    count = 0
+    current = @head
+
+    while current
+      count += 1
+      current = current.next_node
     end
+    
+    count
   end
+    #  if head.data == nil
+    #   @count_node = 0
+    #  else
+    #     @count_node = 1
+    #     current_node = @head
+    #     while current_node.next_node != nil
+    #       current_node = current_node.next_node
+    #       @count_node += 1
+    #     end
+    #  end
+
+    
+    # else
+    #   count += 1 while head.next_node == nil
+    # end
+    # count = 0
+    # while current.next_node.data == nil
+    #   count +=1
+    # end
+    # if @head.data == nil
+    #   0
+    # else
+    #   1
+    # end
+  
 
   def to_string
-   puts head.data
+   if @head == nil
+    nil
+   else
+    current_node = @head
+      until current_node.next_node == nil do
+        string = "#{string} #{current_node.data}"
+        current_node = current_node.next_node
+      end
+    end
+
+    string = "#{string} #{current_node.data}"
+    string.strip
   end
+
+  # def prepend(data)
+  #   new_node = Node.new(data)
+  #   current = @head
+
+  #   if @head == nil
+  #     @head = new_node
+  #   else
+  #     current = @head 
+  #     if current.next_node == nil
+  #       current.next_node = new_node
+  #     end
+    # if @head = nil?
+    #   @head = node_1
+    # end
+    # end
+    # @count_node += 1
+  # end 
+
 end
+
+
+
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -6,7 +6,7 @@ list = LinkedList.new
 describe LinkedList do
 
   describe '#initialize' do
-    it 'is an instance of LinkedList' do
+    xit 'is an instance of LinkedList' do
 
     expect(list).to be_an_instance_of(LinkedList)
     end
@@ -20,51 +20,66 @@ describe LinkedList do
   end
 
   describe "#append" do
-    it "add data to list" do
+    xit "add data to list" do
 
     list.append("doop")
     expect(list.head.data).to eq("doop")
     expect(list.head).to be_an_instance_of(Node)
     expect(list.head.next_node).to eq(nil)
+    list.append("deep")
+    expect(list.head).to be_an_instance_of(Node)
+    expect(list.head.next_node.data).to eq("deep")
+    #this portion was very difficutl, but a huge step forward after figuring it out!# 
+  
+
     end
   end
   
 
   describe "#count#" do
     it "counts the amount of data listed" do
-
+    # expect(list.count).to eq(0)  
+    
     list.append("doop")
     expect(list.count).to eq(1)
+    list.append("deep")
+    expect(list.count).to eq(2)
+    list.append("sooo")
+    list.append("drooo")
+    require 'pry';binding.pry
+    expect(list.count).to eq(4)
+
     end
   end
 
   describe "#to_string" do
-    it "returns all elements in the list" do
+    xit "returns all elements in the list" do
 
-      list.append("doop")
-      expect(list.head.data).to eq("doop")
-    end
-  end
-
-  describe "#head" do
-    it "returns data from next_node" do
-
-      expect(list.head.next_node).to eq(nil)
-    end
-  end
-
-  describe "#append" do
-    it "adds an additional element to the list" do
-      
       list.append("doop")
       list.append("deep")
+
       expect(list.head.data).to eq("doop")
-      expect(list.head).to be_an_instance_of(Node)
-      # expect(list.head.next_node).to be_an_instance_of(Node)
-      expect(list.head.next_node.data).to eq("deep")
+      expect(list.to_string).to eq("doop deep")
+    end
+  end
+  
+  describe "#prepend" do
+    xit "will add nodes to the beginning of the list" do
+      list.append("plop")
+      expect(list.to_string).to eq("plop")
+      list.append("suu")
+      expect(list.to_string).to eq("plop suu")
+      list.prepend("dop")
+
     end
   end
 
 
 end
 
+
+
+#Created a variable regarding the current node that you are on.
+#count += 1
+#Current node = head
+#count until next_node is nil, if not, keep counting
