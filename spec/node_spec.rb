9e36be13a1bc21078c91338a199923e3ed014280
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-07-10 13:54:08 -0500

    test: node class / check org

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,25 +1,28 @@
 require './lib/node'
-node = Node.new("plop")
 
 
 describe Node do
   describe '#initialize' do
     it 'is an instance of node' do
+      node = Node.new("plop")
 
-    expect(node).to be_an_instance_of(Node)
+      expect(node).to be_an_instance_of(Node)
     end
   end
 
   describe "#data" do
     it "returns the data" do
+      node = Node.new("plop")
 
-    expect(node.data).to eq("plop")
+      expect(node.data).to eq("plop")
     end
+
   end
    describe "#next_node" do
     it "returns next_node" do
+      node = Node.new("plop")
 
-    expect(node.next_node).to eq(nil)
+      expect(node.next_node).to eq(nil)
     end
   end
 end
